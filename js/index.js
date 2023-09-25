e50b88081428468d4b963d156cf9505ef960a1ad
Connor Adams <connorvadams@gmail.com>
2023-09-25 15:30:53 -0600

    Adds AZ and rank filters.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 // notify(api_key,"error")
 
+// ! -- Connor's Code --
+
+const coinList = document.querySelector('#results')
+
 const fetchAllCoins = () =>{
     return fetch("https://api.coincap.io/v2/assets/")
     .then(resp => resp.json())
@@ -21,7 +25,50 @@ const getCoinHistory = (coin,interval="m1") =>{
     return fetch(url)
     .then(resp => resp.json())
     .then(body => body.data)
+};
+
+const renderLi = (coin) => {
+    const listedCoin = document.createElement('li');
+    listedCoin.classList.add('listedCoin');
+    listedCoin.dataset.id = coin.id;
+    listedCoin.textContent = `${coin.name} -- ${coin.symbol}`;
+    // listedCoin.addEventListener('click', displayCoinToWindow);
+    coinList.appendChild(listedCoin);
+};
+
+const populateCoinList = (coinsObj) => {
+    for (coin of coinsObj) {
+        renderLi(coin)
+    }
 }
+
+const filterByAZ = (coinsObj) => {
+        const toBeSorted = []
+        for (coin of coinsObj) {
+        toBeSorted.push(coin)
+        }
+        toBeSorted.sort((a, b) => {
+        return a.name.localeCompare(b.name)
+        })
+        return toBeSorted;
+}
+
+document.querySelector('#filter').addEventListener('change', (e) => {
+    coinList.innerHTML = ''
+    switch (e.target.value) {
+        case 'default':
+            fetchAllCoins()
+            .then(populateCoinList)
+            break;
+        case 'alphabetical':
+            fetchAllCoins()
+            .then(filterByAZ)
+            .then(populateCoinList)
+            break;
+    }
+})
+
+
 
 // fetchCoin("BTC").then(console.log)
 
