f79dce54f621914037e4b3d842a3bd35e249f559
Danner Baumgartner <danner.baumgartner@gmail.com>
2023-09-25 18:22:28 -0700

    added price change arrows

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
- //notify(api_key,"error")
+//notify(api_key,"error")
 
 "use strict"
 
@@ -7,122 +7,145 @@
 const coinList = document.querySelector("#results");
 
 const fetchAllCoins = () => {
-  return fetch("https://api.coincap.io/v2/assets/")
-    .then((resp) => resp.json())
-    .then((body) => body.data);
+    return fetch("https://api.coincap.io/v2/assets/")
+        .then((resp) => resp.json())
+        .then((body) => body.data);
 };
 
 const fetchCoin = async (coinName) => {
-  const coins = await fetchAllCoins();
-  for (let coin of coins) {
-    const lowerCoin = coinName.toLowerCase();
-    if (
-      lowerCoin === coin.id.toLowerCase() ||
-      lowerCoin === coin.name.toLowerCase() ||
-      lowerCoin === coin.symbol.toLowerCase()
-    ) {
-      return coin;
+    const coins = await fetchAllCoins();
+    for (let coin of coins) {
+        const lowerCoin = coinName.toLowerCase();
+        if (
+            lowerCoin === coin.id.toLowerCase() ||
+            lowerCoin === coin.name.toLowerCase() ||
+            lowerCoin === coin.symbol.toLowerCase()
+        ) {
+            return coin;
+        }
     }
-  }
 };
 
 const getCoinHistory = (coin, interval = "m1") => {
-  const url = `https://api.coincap.io/v2/assets/${coin}/history?interval=${interval}`;
-  return fetch(url)
-    .then((resp) => resp.json())
-    .then((body) => body.data);
+    const url = `https://api.coincap.io/v2/assets/${coin}/history?interval=${interval}`;
+    return fetch(url)
+        .then((resp) => resp.json())
+        .then((body) => body.data);
 };
 
 const renderLi = (coin) => {
-  const listedCoin = document.createElement("li");
-  listedCoin.classList.add("listedCoin");
-  listedCoin.dataset.id = coin.id;
-  listedCoin.textContent = `${coin.name} -- ${coin.symbol}`;
-  listedCoin.addEventListener('click',() => displayCoin(coin));
-  coinList.appendChild(listedCoin);
+    const listedCoin = document.createElement("li");
+    listedCoin.classList.add("listedCoin");
+    listedCoin.dataset.id = coin.id;
+    listedCoin.textContent = `${coin.name} -- ${coin.symbol}`;
+    listedCoin.addEventListener('click', () => displayCoin(coin));
+    coinList.appendChild(listedCoin);
 };
 
 const populateCoinList = (coinsObj) => {
-  for (let coin of coinsObj) {
-    renderLi(coin);
-  }
+    for (let coin of coinsObj) {
+        renderLi(coin);
+    }
 };
 
 const filterByAZ = (coinsObj) => {
-  const toBeSorted = [];
-  for (let coin of coinsObj) {
-    toBeSorted.push(coin);
-  }
-  toBeSorted.sort((a, b) => {
-    return a.name.localeCompare(b.name);
-  });
-  return toBeSorted;
+    const toBeSorted = [];
+    for (let coin of coinsObj) {
+        toBeSorted.push(coin);
+    }
+    toBeSorted.sort((a, b) => {
+        return a.name.localeCompare(b.name);
+    });
+    return toBeSorted;
 };
 
 const filterByGains = (coinsObj, upOrDown) => {
-  const toBeSorted = [];
-  for (let coin of coinsObj) {
-    toBeSorted.push(coin);
-  }
-  if (upOrDown === 0) {
-    toBeSorted.sort((a, b) => {
-      return parseFloat(b.changePercent24Hr) - parseFloat(a.changePercent24Hr);
-    });
-  } else {
-    toBeSorted.sort((a, b) => {
-      return parseFloat(a.changePercent24Hr) - parseFloat(b.changePercent24Hr);
-    });
-  }
-  return toBeSorted;
-};
-
-const filterByPrice = (coinsObj,upOrDown) =>{
     const toBeSorted = [];
     for (let coin of coinsObj) {
-      toBeSorted.push(coin);
+        toBeSorted.push(coin);
     }
     if (upOrDown === 0) {
-      toBeSorted.sort((a, b) => {
-        return parseFloat(b.priceUsd) - parseFloat(a.priceUsd);
-      });
+        toBeSorted.sort((a, b) => {
+            return parseFloat(b.changePercent24Hr) - parseFloat(a.changePercent24Hr);
+        });
     } else {
-      toBeSorted.sort((a, b) => {
-        return parseFloat(a.priceUsd) - parseFloat(b.priceUsd);
-      });
+        toBeSorted.sort((a, b) => {
+            return parseFloat(a.changePercent24Hr) - parseFloat(b.changePercent24Hr);
+        });
+    }
+    return toBeSorted;
+};
+
+const filterByPrice = (coinsObj, upOrDown) => {
+    const toBeSorted = [];
+    for (let coin of coinsObj) {
+        toBeSorted.push(coin);
+    }
+    if (upOrDown === 0) {
+        toBeSorted.sort((a, b) => {
+            return parseFloat(b.priceUsd) - parseFloat(a.priceUsd);
+        });
+    } else {
+        toBeSorted.sort((a, b) => {
+            return parseFloat(a.priceUsd) - parseFloat(b.priceUsd);
+        });
     }
     return toBeSorted;
 }
 
-const filterByName = (coinName) =>{
-    coinList.innerHTML=""
-    fetchAllCoins().then(coinsObj =>{
+const filterByName = (coinName) => {
+    coinList.innerHTML = ""
+    fetchAllCoins().then(coinsObj => {
         coinsObj.forEach(coin => {
             const lowerCoin = coinName.toLowerCase()
-            if(coin.id.toLowerCase().startsWith(lowerCoin) ||
-            coin.name.toLowerCase().startsWith(lowerCoin) ||
-            coin.symbol.toLowerCase().startsWith(lowerCoin)){
+            if (coin.id.toLowerCase().startsWith(lowerCoin) ||
+                coin.name.toLowerCase().startsWith(lowerCoin) ||
+                coin.symbol.toLowerCase().startsWith(lowerCoin)) {
                 renderLi(coin)
             }
-        }) 
+        })
     })
 }
 
 const fetchCoinImages = (coin) => {
     return fetch('src/CoinAssets.json')
-    .then(resp => resp.json())
-    .then(data => {
-        return data.find(element => element['asset_id'] === coin.symbol)
+        .then(resp => resp.json())
+        .then(data => {
+            return data.find(element => element['asset_id'] === coin.symbol)
+        })
+}
+
+const formatPrice = (price) => {
+    return parseFloat(price).toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: (price > 1) ? 2 : 8,
+    });
+}
+
+const fetchDailyChange = (coinName) =>{
+    return fetchCoin(coinName)
+    .then(coin =>{
+        formatDailyChange(coin.changePercent24Hr)
+        return coin.changePercent24Hr
     })
 }
 
-const formatPrice = (price) =>{
-    return parseFloat(price).toLocaleString('en-US', {
-        style: 'currency',
-        currency: 'USD', 
-        minimumFractionDigits: (price > 1) ? 2 : 8 ,
-      });
-      
-  }
+const formatDailyChange = (priceChange) =>{
+    const slice = (priceChange.slice(0,4))
+    if(slice > 0){
+        document.querySelector(".material-symbols-outlined").classList.remove("downArrow")
+        document.querySelector(".material-symbols-outlined").classList.toggle("upArrow")
+        document.querySelector(".material-symbols-outlined").textContent = "arrow_drop_up"
+
+    }else{
+        document.querySelector(".material-symbols-outlined").classList.remove("upArrow")
+        document.querySelector(".material-symbols-outlined").classList.toggle("downArrow")
+        document.querySelector(".material-symbols-outlined").textContent = "arrow_drop_down"
+    }
+    return slice
+}
+
 
 const displayCoin = (coin) => {
     const title = document.querySelector("#coinName")
@@ -130,67 +153,68 @@ const displayCoin = (coin) => {
     const image = document.querySelector("#coinImg")
     title.textContent = coin.name
     price.textContent = formatPrice(coin.priceUsd)
+    fetchDailyChange(coin.name).then(console.log)
     fetchCoinImages(coin)
-    .then(coinObj => {
-        if (coinObj) {
-          image.src = coinObj.url
-        } else {
-          image.src = 'src/download.jpeg'
-          console.log(coin.symbol)
-        }
-        image.alt = coin.symbol
-  })
+        .then(coinObj => {
+            if (coinObj) {
+                image.src = coinObj.url
+            } else {
+                image.src = 'src/download.jpeg'
+                console.log(coin.symbol)
+            }
+            image.alt = coin.symbol
+        })
 }
 
 
 
 document.querySelector("#filter").addEventListener("change", (e) => {
-  coinList.innerHTML = "";
-  switch (e.target.value) {
-    case "default":
-      fetchAllCoins().then(populateCoinList);
-      break;
+    coinList.innerHTML = "";
+    switch (e.target.value) {
+        case "default":
+            fetchAllCoins().then(populateCoinList);
+            break;
 
-    case "alphabetical":
-      fetchAllCoins().then(filterByAZ).then(populateCoinList);
-      break;
+        case "alphabetical":
+            fetchAllCoins().then(filterByAZ).then(populateCoinList);
+            break;
 
-    case "biggestDailyGains":
-      fetchAllCoins()
-        .then((coinsObj) => {
-          return filterByGains(coinsObj, 0);
-        })
-        .then(populateCoinList);
-      break;
+        case "biggestDailyGains":
+            fetchAllCoins()
+                .then((coinsObj) => {
+                    return filterByGains(coinsObj, 0);
+                })
+                .then(populateCoinList);
+            break;
 
-      case "biggestDailyLosses":
-        fetchAllCoins()
-          .then((coinsObj) => {
-            return filterByGains(coinsObj, 1);
-          })
-          .then(populateCoinList);
-        break;
+        case "biggestDailyLosses":
+            fetchAllCoins()
+                .then((coinsObj) => {
+                    return filterByGains(coinsObj, 1);
+                })
+                .then(populateCoinList);
+            break;
 
         case "priceHighest":
-      fetchAllCoins()
-        .then((coinsObj) => {
-          return filterByPrice(coinsObj, 0);
-        })
-        .then(populateCoinList);
-      break;
+            fetchAllCoins()
+                .then((coinsObj) => {
+                    return filterByPrice(coinsObj, 0);
+                })
+                .then(populateCoinList);
+            break;
 
-      case "priceLowest":
-      fetchAllCoins()
-        .then((coinsObj) => {
-          return filterByPrice(coinsObj, 1);
-        })
-        .then(populateCoinList);
-      break;
-  }
+        case "priceLowest":
+            fetchAllCoins()
+                .then((coinsObj) => {
+                    return filterByPrice(coinsObj, 1);
+                })
+                .then(populateCoinList);
+            break;
+    }
 });
 
 
-document.querySelector("form").addEventListener("submit",(e)=>{
+document.querySelector("form").addEventListener("submit", (e) => {
     e.preventDefault()
     // console.log(e.target.Name)
     filterByName(e.target.Name.value)
